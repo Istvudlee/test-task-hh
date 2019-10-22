1b295a49ed463673faff4d24d161b21f486806c5
Istvudlee <istvudlee@mail.ru>
2019-10-22 11:53:14 +0300

    fix test.js file

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 describe("serchMaxNum", function () {
 
 	it("находит индекс максимального значения массива 1-вариант", function () {
-		assert.equal(serchMaxNumFirst([1, 5, 25, 2, 78, 15, 88]), 6);
+		assert.equal(searchMaxNumFirst([1, 5, 25, 2, 78, 15, 88]), 6);
 	});
 
 	it("находит индекс максимального значения массива 2-вариант", function () {
-		assert.equal(serchMaxNumSecond([1, 5, 25, 2, 98, 15, 88]), 4);
+		assert.equal(searchMaxNumSecond([1, 5, 25, 2, 98, 15, 88]), 4);
 
 	});
 
 	it("находит индекс максимального значения массива 3-вариант", function () {
-		assert.equal(serchMaxNumThird([1, 5, 25, 2, 128, 150, 18]), 5);
+		assert.equal(searchMaxNumThird([1, 5, 25, 2, 128, 150, 18]), 5);
 	});
 });
